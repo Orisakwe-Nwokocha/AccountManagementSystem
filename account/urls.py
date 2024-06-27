5bc2633ab7852f15287c8f5ccc6cdcef6cb4160e
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-06-27 14:57:49 +0100

    added views set for CRUD endpoints

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
 from . import views
 
+router = SimpleRouter()
+router.register('all', views.AccountViewSet)
+print(router.urls)
+
 urlpatterns = [
-    path('all', views.find_all),
-    path('find/<str:account_number>', views.get_account),
-    path('deposit', views.deposit),
-    path('withdraw', views.withdraw),
+    # path('all', views.AccountViewSet.as_view({'get': 'list'})),
+    # path('find/<str:pk>', views.AccountViewSet.as_view({'get': 'find', 'put': 'update', 'delete': 'delete'})),
+    path('', include(router.urls)),
+    path('deposit', views.Deposit.as_view()),
+    path('withdraw', views.Withdraw.as_view()),
 ]
+
+
+
--- account/views.py
+++ account/views.py
@@ -1,78 +1,111 @@
 from decimal import Decimal
 
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Account, Transaction
 from .serializers import AccountSerializer, AccountCreateSerializer
 
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def find_all(request):
-    if request.method == 'GET':
-        accounts = Account.objects.all()
-        serializer = AccountSerializer(accounts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = AccountCreateSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AccountViewSet(ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountCreateSerializer
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def get_account(request, account_number):
-    account = get_object_or_404(Account, pk=account_number)
-    if request.method == 'GET':
-        serializer = AccountSerializer(account)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'PUT':
-        serializer = AccountCreateSerializer(account, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
-        account.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class FindAll(ListCreateAPIView):
+#     queryset = Account.objects.all()
+#     serializer_class = AccountCreateSerializer
+
+    # def get_queryset(self):
+    #     return Account.objects.all()
+    #
+    # def get_serializer_class(self):
+    #     return AccountCreateSerializer
+
+    # @staticmethod
+    # def get(request):
+    #     accounts = Account.objects.all()
+    #     serializer = AccountSerializer(accounts, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # @staticmethod
+    # def post(request):
+    #     serializer = AccountCreateSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["POST"])
-def deposit(request):
-    account_number = request.data['account_number']
-    amount = Decimal(request.data['amount'])
-    account = get_object_or_404(Account, pk=account_number)
-    if amount <= 0.0:
-        return Response(data={"success": False, "message": "Amount must be greater than 0"}, status=status.HTTP_400_BAD_REQUEST)
-    account.balance += amount
-    account.save()
-    Transaction.objects.create(
-        account=account,
-        amount=amount
-    )
-    return Response(data={"success": True, "message": "Transaction successful"}, status=status.HTTP_200_OK)
+# class FindAccount(RetrieveUpdateDestroyAPIView):
+#     queryset = Account.objects.all()
+#     serializer_class = AccountCreateSerializer
+
+    # @staticmethod
+    # def get(request, account_number):
+    #     account = get_object_or_404(Account, pk=account_number)
+    #     serializer = AccountSerializer(account)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # @staticmethod
+    # def put(request, account_number):
+    #     account = get_object_or_404(Account, pk=account_number)
+    #     serializer = AccountSerializer(account, data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # @staticmethod
+    # def delete(request, account_number):
+    #     account = get_object_or_404(Account, pk=account_number)
+    #     account.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(["POST"])
-def withdraw(request):
-    account_number = request.data['account_number']
-    amount = Decimal(request.data['amount'])
-    pin = request.data['pin']
-    account = get_object_or_404(Account, pk=account_number)
-    if account.pin != pin:
-        return Response(data={"success": False, "message": "Invalid pin"}, status=status.HTTP_400_BAD_REQUEST)
-    if amount <= 0:
-        return Response(data={"success": False, "message": "Invalid amount"}, status=status.HTTP_400_BAD_REQUEST)
-    if account.balance < amount:
-        return Response(data={"success": False, "message": "Insufficient funds"}, status=status.HTTP_400_BAD_REQUEST)
-    account.balance -= amount
-    account.save()
-    Transaction.objects.create(
-        account=account,
-        amount=amount,
-        transaction_type='DEB'
-    )
-    return Response(data={"success": True, "message": "Transaction successful"}, status=status.HTTP_200_OK)
+class Deposit(APIView):
+    @staticmethod
+    def post(request):
+        account_number = request.data['account_number']
+        amount = Decimal(request.data['amount'])
+        account = get_object_or_404(Account, pk=account_number)
+        if amount <= 0.0:
+            return Response(data={"success": False, "message": "Amount must be greater than 0"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        account.balance += amount
+        account.save()
+        Transaction.objects.create(
+            account=account,
+            amount=amount
+        )
+        return Response(data={"success": True, "message": "Transaction successful"}, status=status.HTTP_200_OK)
+
+
+class Withdraw(APIView):
+    @staticmethod
+    def post(request):
+        account_number = request.data['account_number']
+        amount = Decimal(request.data['amount'])
+        pin = request.data['pin']
+        account = get_object_or_404(Account, pk=account_number)
+        if account.pin != pin:
+            return Response(data={"success": False, "message": "Invalid pin"}, status=status.HTTP_400_BAD_REQUEST)
+        if amount <= 0:
+            return Response(data={"success": False, "message": "Invalid amount"}, status=status.HTTP_400_BAD_REQUEST)
+        if account.balance < amount:
+            return Response(data={"success": False, "message": "Insufficient funds"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        account.balance -= amount
+        account.save()
+        Transaction.objects.create(
+            account=account,
+            amount=amount,
+            transaction_type='DEB'
+        )
+        return Response(data={"success": True, "message": "Transaction successful"}, status=status.HTTP_200_OK)
